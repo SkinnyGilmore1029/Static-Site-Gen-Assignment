3668a886bb4396c2b05e3dec9f78da8519e02aa2
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-03-30 14:42:10 -0500

    dont know why i am failing

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ import shutil
 from copystatic import copy_files_recursive
 from gencontent import generate_page
 
-
 dir_path_static = "./static"
 dir_path_public = "./public"
 dir_path_content = "./content"
