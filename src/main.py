6df04e64171a9b238ebc63f90772055399526783
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-03-31 07:08:42 -0500

    hopefully this works

--- src/main.py
+++ src/main.py
@@ -1,26 +1,30 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
-
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
     print("Deleting public directory...")
-    # Clean public directory
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    os.makedirs("public", exist_ok=True)
+    # Change to the doc directory
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
+    os.makedirs("docs", exist_ok=True)
 
     # Use your copy_files_recursive function to copy static files to public
-    copy_files_recursive("static", "public")
+    copy_files_recursive("static", "docs")
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public,basepath)
 
 main()
